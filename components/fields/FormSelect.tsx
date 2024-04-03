a0c6def6fc50507eb00a9f0fa7725a5ba45ac5c8
seasierra <seagaerra@gmail.com>
2024-04-03 16:14:43 +0300

    Fix select field

--- components/fields/FormSelect.tsx
+++ components/fields/FormSelect.tsx
@@ -2,28 +2,19 @@
 import { Select, SelectItem } from "@nextui-org/react";
 import { FC } from "react";
 
-export const FormSelect: FC<{ items: (string | boolean)[]; label: string }> = ({
+export const FormSelect: FC<{ items: string[]; label: string }> = ({
   items,
   label,
 }) => {
   return (
     <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
       <Select className="max-w-xs" label={label}>
-        {items
-          .filter((i) => typeof i === "string")
-          .map((item) => (
-            <SelectItem key={item} value={item}>
-              {item}
-            </SelectItem>
-          ))}
-      </Select>
-      {/* <Select label="Select an animal" className="max-w-xs">
-        {props.items.map((item) => (
+        {items.map((item) => (
           <SelectItem key={item} value={item}>
             {item}
           </SelectItem>
         ))}
-      </Select> */}
+      </Select>
     </div>
   );
 };
